eda46055b46304c5861f4265013a43285f81aafc
refailin <lina.refai@mail.utoronto.ca>
2017-09-12 12:13:21 -0400

    editted second function

--- birthdayfun.py
+++ birthdayfun.py
@@ -13,7 +13,7 @@ def hbd(age):
         print("{} years ago, on this very day, something special happened.".format(age))
         i1 = input("Do you want to know what?")
         if i1 == 'yes':
-            return "Your mother got her body back."
+            return "No I don't think so."
         else:
             return "Too bad, but don't worry it wasn't you."
     else:
